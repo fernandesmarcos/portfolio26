959265e85c7fdc8cddbd8a90928443fbced4557b
fernandesmarcos <marcos.fssouza@gmail.com>
2026-03-25 14:41:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const bg = document.querySelector('.bg');
+const page = document.querySelector('.page');
 const links = document.querySelectorAll('.clients a[data-image]');
 const nav = document.querySelector('.clients');
 
@@ -6,9 +7,11 @@ links.forEach(link => {
   link.addEventListener('mouseenter', () => {
     bg.style.backgroundImage = `url('${link.dataset.image}')`;
     bg.classList.add('visible');
+    page.classList.add('is-hovering');
   });
 });
 
 nav.addEventListener('mouseleave', () => {
   bg.classList.remove('visible');
+  page.classList.remove('is-hovering');
 });
